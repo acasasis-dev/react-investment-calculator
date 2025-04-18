2d13712f10e78469e6ccdf02f9590b3e98958650
acasasis-dev <angelito.casasis@gmail.com>
2025-04-18 12:39:09 +0800

    Handle input changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import UserInput from "./components/UserInput";
+import Result from "./components/Result";
+import { calculateInvestmentResults } from "./util/investment";
 
 
 function App() {
+  const inputs = {
+    initial_investment: 0,
+    annual_investment: 0,
+    expected_return: 0,
+    duration: 0
+  }
+  
+  let results = []
+
+  function handleInputChange(inputs) {
+    results = calculateInvestmentResults(inputs)
+  }
+
   return (
     <>
-      <UserInput />
+      <UserInput 
+        onInputChange={ handleInputChange }
+        inputs={ inputs }
+      />
+      <Result />
     </>
   );
 }
