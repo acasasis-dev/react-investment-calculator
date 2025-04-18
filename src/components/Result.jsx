ba824ea3489efb40b1a55e2a383a892be588dbe4
acasasis-dev <angelito.casasis@gmail.com>
2025-04-18 11:46:51 +0800

    Results table init

---
+++ src/components/Result.jsx
@@ -0,0 +1,15 @@
+export default function Result() {
+	return (
+		<>
+			<table id="result">
+				<thead>
+					<td>Year</td>
+					<td>Investment Value</td>
+					<td>Interest (Year)</td>
+					<td>Total Interest</td>
+					<td>Invested Capital</td>
+				</thead>
+			</table>
+		</>
+	)
+}
