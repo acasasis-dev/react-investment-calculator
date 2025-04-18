647accc200c8a640747485042029c6ab17040eb6
acasasis-dev <angelito.casasis@gmail.com>
2025-04-18 12:38:34 +0800

    Made UserInput component lift states

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,24 +1,44 @@
-export default function UserInput() {
+export default function UserInput({ onInputChange, inputs }) {
+	function handleInitInvestmentChange(event) {
+		inputs.initial_investment = event.target.value
+		onInputChange(inputs)
+	}
+
+	function handleAnnualInvestmentChange(event) {
+		inputs.annual_investment = event.target.value
+		onInputChange(inputs)
+	}
+
+	function handleExpectedReturnChange(event) {
+		inputs.expected_return = event.target.value
+		onInputChange(inputs)
+	}
+
+	function handleDurationChange(event) {
+		inputs.duration = event.target.value
+		onInputChange(inputs)
+	}
+
 	return (
 		<section id="user-input">
 			<div className="input-group">
 				<p>
 					<label>Initial Investment</label>
-					<input id="initial-investment" />
+					<input id="initial-investment" onChange={ handleInitInvestmentChange } />
 				</p>
 				<p>
 					<label>Annual Investment</label>
-					<input id="annual-investment" />
+					<input id="annual-investment" onChange={ handleAnnualInvestmentChange } />
 				</p>
 			</div>
 			<div className="input-group">
 				<p>
 					<label>Expected Return</label>
-					<input id="expected-return" />
+					<input id="expected-return" onChange={ handleExpectedReturnChange } />
 				</p>
 				<p>
 					<label>Duration</label>
-					<input id="duration" />
+					<input id="duration" onChange={ handleDurationChange } />
 				</p>
 			</div>
 		</section>
